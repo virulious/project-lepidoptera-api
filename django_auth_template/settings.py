c90877a193a515bb8356156b72475a4817fc2a89
Robert Cronin <Virulious@hotmail.com>
2020-06-26 05:40:21 -0400

    Add CLIENT_ORIGIN

--- django_auth_template/settings.py
+++ django_auth_template/settings.py
@@ -33,7 +33,7 @@ if os.getenv('ENV') == 'development':
       'NAME': DB_NAME,
   }
   DEBUG = True
-  CORS_ORIGIN_WHITELIST = ['http://localhost:7165',]
+  CORS_ORIGIN_WHITELIST = ['http://localhost:7165', 'https://virulious.github.io/']
 else:
   DB = dj_database_url.config()
   DEBUG = False
